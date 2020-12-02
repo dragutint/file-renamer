0806a77b16f5a8c9674916c7244b1d5aa12acc39
Dragutin Todorovic <duda.todorovic.97@gmail.com>
2020-12-02 19:27:19 +0100

    Class rename

--- src/test/java/com/filernm/FileRenamerApplicationTests.java
+++ src/test/java/com/filernm/FileRenamerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileRenamerAppApplicationTests {
+class FileRenamerApplicationTests {
 
 	@Test
 	void contextLoads() {
